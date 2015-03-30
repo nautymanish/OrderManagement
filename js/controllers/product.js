7b52d63be00f44635a207703004910efe6286f36
Manish <manu3k2000@gmail.com>
2015-03-30 23:17:28 +0530

    added form

---
+++ js/controllers/product.js
@@ -0,0 +1,10 @@
+﻿//----------------------------------------------------------------
+// product class
+function product(sku, name, description, price, cal, carot, vitc, folate, potassium, fiber) {
+    this.sku = sku; // product code (SKU = stock keeping unit)
+    this.name = name;
+    this.description = description;
+    this.price = price;
+   
+   
+}
---
+++ js/controllers/store.js
@@ -0,0 +1,33 @@
+﻿//----------------------------------------------------------------
+// store (contains the products)
+//
+
+//
+function store() {
+    this.products = [
+       
+        new product("Shirt", "Shirt", "Mens",2),
+        new product("TShirt", "TShirt", "Mens", 2)
+    ];
+    this.dvaCaption = [
+        "Negligible",
+        "Low",
+        "Average",
+        "Good",
+        "Great"
+    ];
+    this.dvaRange = [
+        "below 5%",
+        "between 5 and 10%",
+        "between 10 and 20%",
+        "between 20 and 40%",
+        "above 40%"
+    ];
+}
+store.prototype.getProduct = function (sku) {
+    for (var i = 0; i < this.products.length; i++) {
+        if (this.products[i].sku == sku)
+            return this.products[i];
+    }
+    return null;
+}
